e551738ebf06ac13b0a5294cf5d22ede35cdf59a
soupaul <soupaul@expediagroup.com>
2023-01-15 20:28:57 +0530

    updated

--- client.js
+++ client.js
@@ -4,12 +4,33 @@ var messageForm = document.getElementById("send_container");
 var messageInput = document.getElementById("message_input");
 var messageContainer = document.getElementById("message_container");
 
+var name = prompt("What is your name?");
+appendMessage("You joined");
+socket.emit("new_user", name);
+
+function appendMessage(message) {
+    var messageElement = document.createElement("div");
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
+
 messageForm.addEventListener("submit", (e) => {
     e.preventDefault();
     var message = messageInput.value;
     socket.emit("send_chat_message", message);
+    messageInput.value = "";
+    appendMessage("You: "+message);
 });
 
 socket.on("new_chat_message", function(data) {
     console.log(data);
+    appendMessage(data.name+" : "+data.message);
 });
+
+socket.on("new_user_connected", function(name) {
+    appendMessage(name+" joined");
+});
+
+socket.on("user_disconnected", function(name) {
+    appendMessage(name+" disconnected");
+})
--- index.js
+++ index.js
@@ -7,10 +7,21 @@ var io = require("socket.io")(8000, {
     }
 });
 
+var users = {};
+
 // start the connection and do whatever you want to do
 io.on("connection", function(socket) {
     socket.on("send_chat_message", function(data) {
         console.log(data);
-        socket.broadcast.emit("new_chat_message", data);
+        console.log(socket.id);
+        socket.broadcast.emit("new_chat_message", {name: users[socket.id], message: data});
     });
+    socket.on("new_user", function(name) {
+        users[socket.id] = name;
+        socket.broadcast.emit("new_user_connected", name);
+    });
+    socket.on("disconnect", function() {
+        socket.broadcast.emit("user_disconnected", users[socket.id]);
+        delete users[socket.id];
+    })
 });
